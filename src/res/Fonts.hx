c19672622e2e4730bdb712ee02f27e4be6da4912
Achmad Aulia <aaulia@gmail.com>
2013-12-17 16:55:10 +0700

    Add Joystix font

--- src/res/Fonts.hx
+++ src/res/Fonts.hx
@@ -3,4 +3,5 @@ import flash.text.Font;
 
 @:font("res/ttf/dina10px.ttf") class DinaFont     extends Font {}
 @:font("res/ttf/kenpixel.ttf") class KenPixelFont extends Font {}
+@:font("res/ttf/joystix.ttf")  class JoystixFont  extends Font {}
 class Fonts { }
